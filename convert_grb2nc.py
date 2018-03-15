0b42e8cae3911933f236350c33324084b4a869a1
Luca Delucchi <lucadeluge@gmail.com>
2018-03-15 15:35:49 +0100

    improved script to convert grib file to netcdf

--- convert_grb2nc.py
+++ convert_grb2nc.py
@@ -7,57 +7,55 @@ Created on Mon Nov 27 11:35:32 2017
 """
 import asyncio
 from asyncio import subprocess
-import datetime
 import fnmatch
 import os
+import argparse
+import bz2
 
-
-IBASEDIR = "/share/data/EU/climatic/DWD/COSMO_REA6/T/3D/extract/"
-OBASEDIR = "/share/data/EU/climatic/DWD/COSMO_REA6/T/3D/convert/"
-
-FGRID = os.path.join(OBASEDIR, "new_grid.txt")
-
-FNAME = "{var}_{date:%Y%m%d_%H}.nc"
-
+GRNAME = "{var}_{date}.grb"
+NCNAME = "{var}_{date}.nc"
 
 CDO = ("cdo -f {ffmt} -b {ftype} -O -P {fprocs} -z {fzip} "
        "remapcon,{fgrid} {finput} {foutput}")
 
 FFMT = "nc4c"
 FTYPE = "F32"
-FPROCS = 4
 FZIP = "zip9"
-FGRID = os.path.join(OBASEDIR, "new_grid.txt")
 
 
-def get_outputs(basedir, fname):
-    """''"""
+def get_outputs(basedir, fname, sep):
+    """"""
     fname, extension = os.path.splitext(fname)
-    var, date, hour = fname.split("_")
-    # print(var, date, hour)
-    info = dict(var=var,
-                date=datetime.datetime.strptime(f"{date}_{hour}", "%Y%m%d_%H"))
-    date = info["date"]
-    conv_dir = os.path.join(basedir,
-                            date.strftime("%Y"), date.strftime("%m"))
+    vals = fname.split(sep)
+    if vals[-1] == 'bz2':
+        vals = vals[:-1]
+    if len(vals) != 4:
+        print(f"FNAME: {fname} - {basedir}")
+        return "", ""
+    var, typ, date = vals[:3]
+    conv_dir = os.path.join(basedir, f"{var}")
     os.makedirs(conv_dir, exist_ok=True)
-    # info["extension"] = "nc"
-    return conv_dir, FNAME.format(**info)
+    return conv_dir, GRNAME.format(var=var, date=date), NCNAME.format(var=var, date=date)
 
 
 asyncio.coroutine
-def convert_all(ibasedir, obasedir, fpattern, **copts):
+def convert_all(ibasedir, obasedir, fpattern, sep, **copts):
     print("Converting:")
     for base, dirs, files in os.walk(ibasedir):
         for ifile in sorted(fnmatch.filter(files, fpattern)):
-            odir, ofile = get_outputs(obasedir, ifile)
+            odir, grfile, ofile = get_outputs(obasedir, ifile, sep)
             opath = os.path.join(odir, ofile)
+            gpath = os.path.join(base, grfile)
+            ipath = os.path.join(base, ifile)
             if os.path.exists(opath):
                 print(f"File {ifile} already exists: {ofile}")
             else:
                 print(f"File {ifile}: {ofile}")
-                cdo = CDO.format(finput=os.path.join(base, ifile),
-                                 foutput=opath, **copts)
+                with open(gpath, 'wb') as grbfile, bz2.BZ2File(ipath, 'rb') as file:
+                    grbfile.write(file.read())
+                grbfile.close()
+                file.close()
+                cdo = CDO.format(finput=gpath, foutput=opath, **copts)
                 print(cdo)
                 process = yield from asyncio.create_subprocess_shell(
                     cdo, stdout=subprocess.PIPE, stderr=subprocess.PIPE
@@ -65,12 +63,55 @@ def convert_all(ibasedir, obasedir, fpattern, **copts):
                 stdout, stderr = yield from process.communicate()
                 print(stdout)
                 print(stderr)
+                os.remove(gpath)
+
             print("-" * 30)
 
 
+def main(args):
+    FGRID = os.path.join(args.OUTPUT_DIR, "grid.txt")
+    if not os.path.exists(FGRID):
+        raise IOError(f"{FGRID} doesn't exist")
+    if args.year:
+        patt = "*{ye}*.grb.bz2".format(ye=args.year)
+    else:
+        patt = "*.grb.bz2"
+    if args.variables:
+        for var in args.variables[0].split(','):
+            patt = "{va}{pa}".format(va=var, pa=patt)
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(convert_all(ibasedir=args.INPUT_DIR,
+                                                obasedir=args.OUTPUT_DIR,
+                                                fpattern=patt, sep=args.sep,
+                                                ffmt=FFMT, ftype=FTYPE,
+                                                fprocs=args.nproc,
+                                                fzip=FZIP, fgrid=FGRID))
+    else:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(convert_all(ibasedir=args.INPUT_DIR,
+                                            obasedir=args.OUTPUT_DIR,
+                                            fpattern=patt, sep=args.sep,
+                                            ffmt=FFMT, ftype=FTYPE,
+                                            fprocs=args.nproc,
+                                            fzip=FZIP, fgrid=FGRID))
+
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(convert_all(ibasedir=IBASEDIR, obasedir=OBASEDIR,
-                                        fpattern="*.grb", ffmt=FFMT,
-                                        ftype=FTYPE, fprocs=FPROCS,
-                                        fzip=FZIP, fgrid=FGRID))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("INPUT_DIR", help="The input directory where to "
+                        "store the downloaded data")
+    parser.add_argument("OUTPUT_DIR", help="The output directory where to "
+                        "store the downloaded data")
+    parser.add_argument("-f", "--file", help="The full path to a txt file "
+                        "contains the properties of the new grid.")
+    parser.add_argument("-v", "--variables", help="The selected variables to"
+                        " process", nargs='+')
+    parser.add_argument("-n", "--nproc", type=int, default=2,
+                        help="Processors' number to use (default: %(default)s)")
+    parser.add_argument("-y", "--year", type=int, help="Year to analyze")
+    parser.add_argument("-s", "--sep", default=".",
+                        help="The separator used into the file name "
+                        "(default: %(default)s)")
+    parser.add_argument("-r", "--remove", action="store_true",
+                        help="Remove original files")
+    args = parser.parse_args()
+    main(args)
